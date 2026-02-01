150ba9d69216c570ca2919e1a4707fe097d8e9da
Phat Phan Tan <lqm550@gmail.com>
2026-02-01 17:05:09 +0700

    fix

--- config/google-oauth.ts
+++ config/google-oauth.ts
@@ -1,14 +1,11 @@
 export const googleOAuthConfig = {
   web: {
-    client_id:
-      process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID ||
-      "",
+    client_id: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || "",
     project_id: "csc13106-credit-app",
     auth_uri: "https://accounts.google.com/o/oauth2/auth",
     token_uri: "https://oauth2.googleapis.com/token",
     auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-    client_secret:
-      process.env.GOOGLE_CLIENT_SECRET || "",
+    client_secret: process.env.GOOGLE_CLIENT_SECRET || "",
     redirect_uris: [
       "http://localhost:3000",
       "http://localhost:3000/google/callback",
